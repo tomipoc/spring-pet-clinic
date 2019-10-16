4b8263525a9a9870b1e6bc81e9ee190a6ae6e667
Enyedi Tamás <tamas.enyedi@loxon.eu>
2019-10-16 12:24:22 +0200

    refactor to common crud interface. closes #11

--- pet-clinic-data/src/main/java/com/tomipoc/pocpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/tomipoc/pocpetclinic/services/PetService.java
@@ -2,12 +2,6 @@ package com.tomipoc.pocpetclinic.services;
 
 import com.tomipoc.pocpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/tomipoc/pocpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/tomipoc/pocpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.tomipoc.pocpetclinic.services;
 
 import com.tomipoc.pocpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
 }
