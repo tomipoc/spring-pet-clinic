3931d67e5af54971852aa57e81e616b2d0bc6f8e
Enyedi Tamás <tamas.enyedi@loxon.eu>
2019-10-16 12:49:02 +0200

    Impl Map baser Services. closes #6

---
+++ pet-clinic-data/src/main/java/com/tomipoc/pocpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,28 @@
+package com.tomipoc.pocpetclinic.services.map;
+
+import com.tomipoc.pocpetclinic.model.Pet;
+import com.tomipoc.pocpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override public void deleteById( Long id ) {
+        super.deleteById(id);
+    }
+
+    @Override public void delete( Pet object ) {
+        super.delete(object);
+    }
+
+    @Override public Pet save( Pet object ) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override public Pet findById( Long id ) {
+        return super.findById(id);
+    }
+}
